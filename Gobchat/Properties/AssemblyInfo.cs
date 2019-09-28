c70e407ec952f83224307f81254384cf1e838ddc
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2019-09-28 21:51:07 +0200

    Update version number

--- Gobchat/Properties/AssemblyInfo.cs
+++ Gobchat/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
