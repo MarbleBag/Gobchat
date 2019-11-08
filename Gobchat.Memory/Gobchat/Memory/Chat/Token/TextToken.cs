8e71100f5c7880e0d3b883e36dc733bde1363a82
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2019-11-08 19:39:25 +0100

    Move Memory Chat tokens to own namespace

---
+++ Gobchat.Memory/Gobchat/Memory/Chat/Token/TextToken.cs
@@ -0,0 +1,36 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2019 MarbleBag
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *******************************************************************************/
+
+namespace Gobchat.Memory.Chat.Token
+{
+    public class TextToken : IChatlogToken
+    {
+        public byte[] Data { get; }
+
+        public TextToken(byte[] data)
+        {
+            this.Data = data;
+        }
+
+        public string GetText()
+        {
+            return System.Text.Encoding.UTF8.GetString(Data);
+        }
+
+        public override string ToString()
+        {
+            return $"Text[{GetText()}]";
+        }
+    }
+
+}
---
+++ Gobchat.Memory/Gobchat/Memory/Chat/Token/UnknownLinkToken.cs
@@ -0,0 +1,33 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2019 MarbleBag
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *******************************************************************************/
+
+namespace Gobchat.Memory.Chat.Token
+{
+    public class UnknownLinkToken : IChatlogToken
+    {
+        public string Trigger { get; }
+        public string LinkType { get; }
+
+        public UnknownLinkToken(string trigger, string linkType)
+        {
+            Trigger = trigger;
+            LinkType = linkType;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(this.GetType)}[0x{Trigger} | 0x{LinkType}]";
+        }
+    }
+
+}
