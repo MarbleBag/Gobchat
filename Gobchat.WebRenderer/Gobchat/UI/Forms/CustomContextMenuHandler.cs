d813eff1b4139e85040ca455732de351ac72cb66
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2019-11-08 11:09:58 +0100

    Rename WebRenderManager to CEFManager

---
+++ Gobchat.WebRenderer/Gobchat/UI/Forms/CustomContextMenuHandler.cs
@@ -0,0 +1,43 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2019 MarbleBag
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *******************************************************************************/
+
+using CefSharp;
+
+namespace Gobchat.UI.Forms
+{
+    // Context menu does not work correctly and doesn't show up at the position of the right click. For now, it will be disabled.
+    internal class CustomContextMenuHandler : IContextMenuHandler
+    {
+        // howto: https://github.com/cefsharp/CefSharp/blob/935d3900ba2147f4786386596b62339087ff61b0/CefSharp.WinForms.Example/Handlers/MenuHandler.cs#L15
+        void IContextMenuHandler.OnBeforeContextMenu(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+        {
+            // context menu doesn't show up when it has no entries.
+            model.Clear();
+        }
+
+        bool IContextMenuHandler.OnContextMenuCommand(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+        {
+            return false;
+        }
+
+        void IContextMenuHandler.OnContextMenuDismissed(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame)
+        {
+            
+        }
+
+        bool IContextMenuHandler.RunContextMenu(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+        {
+            return false;
+        }
+    }
+}
