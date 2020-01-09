2a82f4f9ebded9768946c2902a429592a79e1f64
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2020-01-09 03:01:17 +0100

    Add first prototype for window tracking

--- Gobchat.Memory/MemoryProcessor.cs
+++ Gobchat.Memory/MemoryProcessor.cs
@@ -21,14 +21,16 @@ namespace Gobchat.Memory
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
-        private readonly FFXIVProcessFinder processFinder = new FFXIVProcessFinder();
+        private readonly FFXIVProcessFinder _processFinder = new FFXIVProcessFinder();
 
-        private readonly Chat.ChatlogReader chatlogProcessor = new Chat.ChatlogReader();
-        private readonly Chat.ChatlogBuilder chatlogBuilder = new Chat.ChatlogBuilder();
+        private readonly Chat.ChatlogReader _chatlogProcessor = new Chat.ChatlogReader();
+        private readonly Chat.ChatlogBuilder _chatlogBuilder = new Chat.ChatlogBuilder();
 
-        public bool FFXIVProcessValid { get { return processFinder.FFXIVProcessValid; } }
+        private readonly Window.WindowObserver _windowScanner = new Window.WindowObserver();
 
-        public int FFXIVProcessId { get { return processFinder.FFXIVProcessId; } }
+        public bool FFXIVProcessValid { get { return _processFinder.FFXIVProcessValid; } }
+
+        public int FFXIVProcessId { get { return _processFinder.FFXIVProcessId; } }
 
         public bool ChatLogAvailable { get { return Sharlayan.Scanner.Instance.Locations.ContainsKey(Sharlayan.Signatures.ChatLogKey); } }
 
@@ -50,6 +52,11 @@ namespace Gobchat.Memory
         public event EventHandler<ProcessChangeEventArgs> ProcessChangeEvent;
 
         /// <summary>
+        /// Fired when the currently tracked FFXIV window is moved into the foreground or into the background
+        /// </summary>
+        public event EventHandler<WindowFocusChangedEventArgs> WindowFocusChangedEvent;
+
+        /// <summary>
         /// Fired when new FFXIV chatlog entries are read
         /// </summary>
         public event EventHandler<Chat.ChatlogEventArgs> ChatlogEvent;
@@ -63,6 +70,15 @@ namespace Gobchat.Memory
                 else
                     logger.Warn(e.Exception, () => $"Memory error in {e.Sender}");
             };
+
+            _windowScanner.ActiveWindowChangedEvent += OnEvent_ActiveWindowChangedEvent;
+        }
+
+        private void OnEvent_ActiveWindowChangedEvent(object sender, Window.WindowObserver.ActiveWindowChangedEventArgs e)
+        {
+            if (!FFXIVProcessValid)
+                return;
+            WindowFocusChangedEvent?.Invoke(this, new WindowFocusChangedEventArgs(e.ProcessId == FFXIVProcessId));
         }
 
         public void Update()
@@ -70,7 +86,7 @@ namespace Gobchat.Memory
             CheckProcess();
             if (FFXIVProcessValid)
             {
-                var logs = chatlogProcessor.Query();
+                var logs = _chatlogProcessor.Query();
                 if (logs.Count > 0 && ChatlogEvent != null)
                 {
                     List<Chat.ChatlogItem> items = new List<Chat.ChatlogItem>();
@@ -78,7 +94,7 @@ namespace Gobchat.Memory
                     {
                         try
                         {
-                            items.Add(chatlogBuilder.Process(item));
+                            items.Add(_chatlogBuilder.Process(item));
                         }
                         catch (Chat.ChatBuildException e)
                         {
@@ -100,7 +116,7 @@ namespace Gobchat.Memory
         private void CheckProcess()
         {
             //TODO MUTEX
-            var processChanged = processFinder.CheckProcess();
+            var processChanged = _processFinder.CheckProcess();
             if (!processChanged)
                 return; //nothing to do
 
---
+++ Gobchat.Memory/Window/WindowObserver.cs
@@ -0,0 +1,101 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gobchat.Memory.Window
+{
+    // based on https://stackoverflow.com/questions/4372055/detect-active-window-changed-using-c-sharp-without-polling
+    public sealed class WindowObserver : IDisposable
+    {
+        private readonly static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0x00;
+        private const uint EVENT_SYSTEM_FOREGROUND = 0x03;
+        private const uint EVENT_SYSTEM_MINIMIZEEND = 0x17;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hwnd, out uint lpdwProcessId);
+
+        public event EventHandler<ActiveWindowChangedEventArgs> ActiveWindowChangedEvent;
+
+        private WinEventDelegate _wineventDelegate = null;
+        private IntPtr _foregroundHook;
+        private IntPtr _minimizeHook;
+
+        public WindowObserver()
+        {
+            _wineventDelegate = new WinEventDelegate(WinEventProc);
+            _foregroundHook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, _wineventDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+            if (IntPtr.Zero == _foregroundHook)
+                logger.Error("Unable to register window foreground hook!");
+
+            _minimizeHook = SetWinEventHook(EVENT_SYSTEM_MINIMIZEEND, EVENT_SYSTEM_MINIMIZEEND, IntPtr.Zero, _wineventDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+            if (IntPtr.Zero == _minimizeHook)
+                logger.Error("Unable to register window minimizeend hook!");
+        }
+
+        public void Dispose()
+        {
+            if (IntPtr.Zero != _foregroundHook)
+            {
+                UnhookWinEvent(_foregroundHook);
+                _foregroundHook = IntPtr.Zero;
+            }
+
+            if (IntPtr.Zero != _minimizeHook)
+            {
+                UnhookWinEvent(_minimizeHook);
+                _minimizeHook = IntPtr.Zero;
+            }
+        }
+
+        public string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        private void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            //var windowTitle = GetActiveWindowTitle();
+            GetWindowThreadProcessId(hwnd, out var processId);
+            ActiveWindowChangedEvent?.Invoke(this, new ActiveWindowChangedEventArgs(processId));
+        }
+
+        public sealed class ActiveWindowChangedEventArgs : EventArgs
+        {
+            public uint ProcessId { get; }
+
+            public ActiveWindowChangedEventArgs(uint processId)
+            {
+                ProcessId = processId;
+            }
+        }
+    }
+}
