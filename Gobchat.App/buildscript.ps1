57b04a6817a8f83f52d051874c8fd6d1630ca2bb
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2020-05-13 22:11:51 +0200

    Improved build script

--- Gobchat.App/buildscript.ps1
+++ Gobchat.App/buildscript.ps1
@@ -1,4 +1,27 @@
-﻿
+﻿function CreatePathSibling([string] $Path, [string] $Sibling){
+	return Join-Path (Split-Path -parent $Path) $Sibling
+}
+
+function DeleteIfExists([string] $Path){
+	if(Test-Path $Path){
+		Write-Host "Deleting: $Path"
+		Remove-Item -Recurse -Force  $Path
+	}
+}
+
+function MakeAndDeleteDirectory([string] $Path){
+	DeleteIfExists $Path
+	New-Item -Path $Path -ItemType directory
+}
+
+function RenameAndDeleteDirectory([string] $Path, [string] $NewName){
+	$NewPath = CreatePathSibling $Path $NewName
+	DeleteIfExists $NewPath
+	Rename-Item -Path $Path -NewName $NewName
+	return $NewPath
+}
+
+
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $7zipPath = "F:\Program Files\7-Zip\7z.exe"
 
@@ -8,9 +31,13 @@ if(-Not (Test-Path $releaseFolder)){
 	exit 1
 }
 
-#do a clean build
+#Remove any old build
+Write-Host "Renaming release folder ..."
+$releaseFolder = RenameAndDeleteDirectory $releaseFolder "Gobchat"
+$debugFolder = CreatePathSibling $releaseFolder "GobchatDebug"
+MakeAndDeleteDirectory $debugFolder
 
-#Not the best
+#Deletes all folders except for the ones named in #allowedFolders
 Write-Host "Deleting all the stuff no one cares about ..."
 $allowedFolders = @("resources")
 Get-ChildItem -Path $releaseFolder | 
@@ -21,15 +48,23 @@ Get-ChildItem -Path $releaseFolder |
 		}
 	}
 
+#Delete downloadable content	
 Remove-Item -Recurse -Force "$releaseFolder\resources\sharlayan" -ErrorAction SilentlyContinue
 
+#Remove all .log files
 Get-ChildItem -Path $releaseFolder -Filter  *.log | 
 	ForEach-Object {
 		Remove-Item -Recurse -Force  $_.FullName -ErrorAction SilentlyContinue	
 	}
+	
+#Move any .pdb files
+Get-ChildItem -Path $releaseFolder -Filter  *.pdb | 
+	ForEach-Object {
+		Move-Item -Path $_.FullName -Destination $debugFolder -Force -ErrorAction SilentlyContinue
+	}
 
 try{
-
+	#copy documents to release
 	Write-Host "Filling docs with important stuff ..."
 	$rld = "$releaseFolder\docs"
 	$ccc = @(
@@ -51,26 +86,38 @@ try{
 	exit 1
 }
 
+#generate content list
 & "$scriptPath\generate-content-list.ps1" $releaseFolder
 
-$text = [System.IO.File]::ReadAllText("$PWD\Properties\AssemblyInfo.cs");
-$hasMatch = $text -match '\[assembly: AssemblyVersion\("([0-9]+\.[0-9]+\.[0-9]+)\.([0-9]+)"\)'
-if(-Not $hasMatch){
+function GetApplicationVersion(){
+	$text = [System.IO.File]::ReadAllText("$PWD\Properties\AssemblyInfo.cs");
+	$hasMatch = $text -match '\[assembly: AssemblyVersion\("([0-9]+\.[0-9]+\.[0-9]+)\.([0-9]+)"\)'
+	if(-Not $hasMatch){
+		Write-Error "Unable to find app version"
+		exit 1
+		return $null
+	}
+	
+	$appVersion = $Matches[1]
+	$appPrerelease = $Matches[2]
+	if( [int]::Parse($appPrerelease) -gt 0 ){
+		$appVersion = "$appVersion-$appPrerelease"
+	}
+	
+	return $appVersion
+}
+
+$appVersion = GetApplicationVersion
+if (!$appVersion) { 
 	Write-Error "Unable to find app version"
 	exit 1
 }
 
-$appVersion = $Matches[1]
-$appPrerelease = $Matches[2]
-if( [int]::Parse($appPrerelease) -gt 0 ){
-	$appVersion = "$appVersion-$appPrerelease"
-}
-
-Write-Host "Updating version number in about.html to $appVersion ..."
-$aboutFile = "$releaseFolder\resources\ui\config\about.html"
-(Get-Content $aboutFile) `
-    -replace '\$\${appversion}', $appVersion |
-  Out-File $aboutFile -encoding utf8
+#Write-Host "Updating version number in about.html to $appVersion ..."
+#$aboutFile = "$releaseFolder\resources\ui\config\about.html"
+#(Get-Content $aboutFile) `
+#    -replace '\$\${appversion}', $appVersion |
+#  Out-File $aboutFile -encoding utf8
 
 Write-Host "Setting log level in nlog.config to info ..."
 Remove-Item -Force "$releaseFolder\NLog.config" -ErrorAction SilentlyContinue
@@ -81,31 +128,24 @@ Rename-Item -Path "$releaseFolder\NLog-Release.config" -NewName "NLog.config"
 #	-replace '<logger name="\*" minlevel=".+" writeTo="file" />', '<logger name="*" minlevel="info" writeTo="file" />' |
 #	Out-File $nlogFile -encoding utf8
   
-Write-Host "Renaming release folder ..."
-$gobFolder = Join-Path (Split-Path -parent $releaseFolder) "Gobchat"
-if(Test-Path $gobFolder){
-	Remove-Item -Recurse -Force  $gobFolder
-}
-
-Rename-Item -Path $releaseFolder -NewName "Gobchat"
-
 if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
     throw "7zip not found at '$7zipPath' Unable to pack release"
 }
 #Set-Alias 7zip $7zipPath
 
-$archiveName = "gobchat-$appVersion.zip"
-$archive = Join-Path (Split-Path -parent $gobFolder) $archiveName
-if (Test-Path $archive) {
-	Write-Host "Deleting old archive"
-	Remove-Item $archive
-}
+$archiveRelease = CreatePathSibling $releaseFolder "gobchat-$appVersion.zip"
+$archiveDebug = CreatePathSibling $debugFolder "gobchat-debug-$appVersion.zip"
+
+DeleteIfExists $archiveRelease
+DeleteIfExists $archiveDebug
 
 Write-Host "Packing release as $archiveName ..."
-& $7zipPath a -mx=9 $archive $gobFolder
+& $7zipPath a -mx=9 $archiveRelease $releaseFolder
+& $7zipPath a -mx=9 $archiveDebug $debugFolder
 
 $outputLocation = (Split-Path -parent $scriptPath)
 Write-Host "Moving package to $outputLocation ..."
-Move-Item -Path $archive -Destination $outputLocation -Force
+Move-Item -Path $archiveRelease -Destination $outputLocation -Force
+Move-Item -Path $archiveDebug -Destination $outputLocation -Force
 
-Write-Host "$archiveName ready to be released"
+Write-Host "$archiveName ready for release"
