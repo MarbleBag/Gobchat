35a7ac90a95861aa73653ff3acceb46827a6a3fa
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2020-05-30 02:43:22 +0200

    Set version to 1.6.3

--- Gobchat.App/Properties/AssemblyInfo.cs
+++ Gobchat.App/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Pre-Release (Used for betas)
 //
-[assembly: AssemblyVersion("1.6.2.0")]
-[assembly: AssemblyFileVersion("1.6.2.0")]
+[assembly: AssemblyVersion("1.6.3.0")]
+[assembly: AssemblyFileVersion("1.6.3.0")]
