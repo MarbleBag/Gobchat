55c8f66f581dd6b507bd14f397b5536d63207029
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2020-06-11 13:49:10 +0200

    Add CEF ShowDefTool on keypress to debug release

---
+++ Gobchat.WebRenderer/Forms/CustomKeyboardHandler.cs
@@ -0,0 +1,39 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2019-2020 MarbleBag
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *******************************************************************************/
+
+using CefSharp;
+using System.Windows.Forms;
+
+namespace Gobchat.UI.Forms
+{
+    internal class CustomKeyboardHandler : IKeyboardHandler
+    {
+        public bool OnKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey)
+        {
+#if DEBUG
+            var winKey = (Keys)windowsKeyCode;
+            if (winKey == Keys.F && modifiers == CefEventFlags.ControlDown)
+            {
+                browser.ShowDevTools();
+                return true;
+            }
+#endif
+            return false;
+        }
+
+        public bool OnPreKeyEvent(IWebBrowser chromiumWebBrowser, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
+        {
+            return false;
+        }
+    }
+}
