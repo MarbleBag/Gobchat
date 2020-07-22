9e4f553d2c145a4aae40480eb390af7667b43139
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2020-07-22 16:26:18 +0200

    Move js from gobchat.html to new file

---
+++ Gobchat.App/resources/ui/gobchat-test.js
@@ -0,0 +1,50 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2019-2020 MarbleBag
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *******************************************************************************/
+
+'use strict'
+
+jQuery(function ($) {
+    function runTest() {
+        function fireMessageEvent(e) {
+            const msg = { channel: e.channel || Gobchat.ChannelEnum.PARTY, source: e.source || "Browser", message: e.message || "Empty" }
+            GobchatAPI.sendChatMessage(msg.channel, msg.source, msg.message)
+        }
+
+        const textMessages = [
+            { channel: Gobchat.ChannelEnum.PARTY, message: '"Say" ((ooc)) *emote* mention' }, //base test
+            { channel: Gobchat.ChannelEnum.PARTY, message: '"Mention contained in say"' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '((Mention contained in ooc))' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '*Mention contained in emote*' },
+            { channel: Gobchat.ChannelEnum.SAY, message: '"Some speech", followed by an emote, "and some more speech."' },
+            { channel: Gobchat.ChannelEnum.SAY, message: 'Display of some special html characters: < > | <<  ≫ « »' },
+            { channel: Gobchat.ChannelEnum.EMOTE, message: 'Display of some special ffxiv characters: \uE0BB' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '"Say that\'s divided by some *emote characters* but should still be displayed correctly."' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '*An emote mixed with "for some important stuff" but contained in an emote*' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: 'Ooc mixed inbetween "some ((ooc)) say" *or ((ooc)) emote*' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: 'Ooc mixed inbetween "some ((ooc)) say" *or ((ooc)) emote*' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '"Mixed stuff *inbetween but the enclosing character* was forgotten.' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '»Say mixed with different types«, of symbols, »to see if they work«' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '«Say mixed with different types», of symbols, «to see if they work»' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: 'Incomplete tokens(' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: 'Incomplete tokens)' },
+            { channel: Gobchat.ChannelEnum.PARTY, message: '((Incomplete tokens)' },
+        ]
+
+        textMessages.forEach(e => fireMessageEvent(e))
+
+        const fillMe = Array.from({ length: 20 }, (v, k) => `|Text1 | <Text 2> "Say" ((ooc)) *emote*   | oof |  Say, *emote* and ((ooc)) - ${k + 1} <emote> \u226Btext long\u226A`).map(e => { return { message: e, source: "Di'e I'of-oOdin" } })
+        fillMe.forEach(e => fireMessageEvent(e))
+    }
+
+    window.setTimeout(runTest, 10000);
+})
---
+++ Gobchat.App/resources/ui/gobchat.js
@@ -0,0 +1,29 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2019-2020 MarbleBag
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *******************************************************************************/
+
+'use strict'
+
+/* Resize display */
+document.addEventListener("OverlayStateUpdate", function (e) {
+    if (!e.detail.isLocked)
+        document.documentElement.classList.add("gob-resize-active");
+    else
+        document.documentElement.classList.remove("gob-resize-active");
+});
+
+jQuery(function ($) {
+    window.chatManager = new Gobchat.GobchatManager("chatbox_content")
+    window.chatManager.init()
+
+    GobchatAPI.setUIReady(true)
+})
