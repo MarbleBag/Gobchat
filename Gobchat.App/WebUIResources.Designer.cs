b5454e405f2abd9dfc118127115fd6d9c110aae8
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2022-07-27 13:26:44 +0200

    Add missing locale entry
    - main.chat.channel.npc-dialog.tooltip

--- Gobchat.App/WebUIResources.Designer.cs
+++ Gobchat.App/WebUIResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Gobchat {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class WebUIResources {
@@ -2472,6 +2472,15 @@ namespace Gobchat {
         internal static string main_chat_channel_npc_dialog {
             get {
                 return ResourceManager.GetString("main.chat.channel.npc-dialog", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        internal static string main_chat_channel_npc_dialog_tooltip {
+            get {
+                return ResourceManager.GetString("main.chat.channel.npc-dialog.tooltip", resourceCulture);
             }
         }
         
