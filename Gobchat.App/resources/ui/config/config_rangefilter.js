b5d4666f8dd77dec711522915fab34d31afa5f39
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2022-12-07 13:51:11 +0100

    Add config page

---
+++ Gobchat.App/resources/ui/config/config_rangefilter.js
@@ -0,0 +1,19 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2019-2022 MarbleBag
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *******************************************************************************/
+
+'use strict';
+
+(function (undefined) {
+    const binding = GobConfigHelper.makeDatabinding(gobconfig)
+
+}());
