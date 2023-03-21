8b2c3d56950a734add34a5800efd569f1bee939c
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2023-03-21 01:55:37 +0100

    Initial project for own updater

---
+++ GobUpdater/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace GobUpdater
+{
+    public sealed class Manager
+    {
+        public static Manager Instance { get; } = new Manager();
+
+        public Manager() {
+            Debug.WriteLine(Assembly.GetExecutingAssembly().FullName);
+            Debug.WriteLine(Assembly.GetCallingAssembly().FullName);
+            Debug.WriteLine(Assembly.GetEntryAssembly()?.FullName);
+            Debug.WriteLine(Assembly.GetAssembly(typeof(Manager))?.FullName);
+        }
+
+        public void CheckForUpdates()
+        {
+
+        }
+
+        public void IsUpdatePending()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+
+    }
+}
