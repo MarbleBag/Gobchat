9bddaedd021311b12dc5a347d4ec7f1b3d6e336d
MarbleBag <12249693+MarbleBag@users.noreply.github.com>
2023-03-21 02:48:52 +0100

    Update copyright

--- Gobchat.App/resources/ui/config/config.ts
+++ Gobchat.App/resources/ui/config/config.ts
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2019-2022 MarbleBag
+ * Copyright (C) 2019-2023 MarbleBag
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License as published by the Free
--- Gobchat.Memory/Properties/AssemblyInfo.cs
+++ Gobchat.Memory/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gobchat.Memory")]
-[assembly: AssemblyCopyright("Copyright © 2019-2022 MarbleBag")]
+[assembly: AssemblyCopyright("Copyright © 2019-2023 MarbleBag")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
